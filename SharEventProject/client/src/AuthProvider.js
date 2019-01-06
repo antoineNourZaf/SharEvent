9c1644b168aee3e72d5b39f29587e3e9e61632e0
BLADE7FOLD\Nathan <nathan.gonzalezmontes@heig-vd.ch>
2019-01-06 13:38:07 +0100

    More Testing

--- SharEventProject/client/src/AuthProvider.js
+++ SharEventProject/client/src/AuthProvider.js
@@ -24,7 +24,7 @@ class AuthProvider extends Component {
       axios.get('/api/me', {
         headers: {
           Authorization: `bearer ${token}`,
-          Accept: `https://fast-refuge-14566.herokuapp.com/`,
+          Accept: `https://fast-refuge-14566.herokuapp.com`,
         }
       })
         .then(response => {
