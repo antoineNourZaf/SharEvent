e739bb950ce741e42bf2ffefd3fca8cc6fb2ff6c
BLADE7FOLD\Nathan <nathan.gonzalezmontes@heig-vd.ch>
2019-01-06 13:40:43 +0100

    More Testing

--- SharEventProject/api/routes/api.js
+++ SharEventProject/api/routes/api.js
@@ -17,6 +17,10 @@ const authenticationRequired = passport.authenticate('jwt', { session: false });
  * see: http://www.passportjs.org/docs/authenticate/#custom-callback
  */
 const authentication = (req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
   return passport.authenticate('jwt', { session: false }, (err, user, info) => {
     if (err) { next(err) }
     req.user = user || null;
